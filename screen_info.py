92bbca84ab857c1e3a8f21bcee86fe1b5540739d
DaltonKipp <daltonkipp@gmail.com>
2022-02-18 17:04:01 -0700

    Formatting

--- screen_info.py
+++ screen_info.py
@@ -3,23 +3,24 @@ import screeninfo
 from screeninfo import get_monitors
 
 def main():
-    # Gets Screen Dimensions
-    screen = tk.Tk()
-    #screenWidth = screen.winfo_screenwidth()
-    #screenHeight = screen.winfo_screenheight()
-    #print('Screen Height: {}'.format(screenHeight))
-    #print('Screen Width: {}'.format(screenWidth))
-    for m in get_monitors():
-        print('\n'+str(m)+'\n')
+    # for m in get_monitors():
+    #    print('\n'+str(m)+'\n')
+
     # Gets screen dimensions from primary monitor
     for m in get_monitors():
-        if m.is_primary:
-            print("\nPrimary Display || Screen Width: {0:^4d}      || Screen Height: {1:^4d}      ||".format(m.width,m.height))
-            print("                || Screen Width (mm): {0:^4d} || Screen Height (mm): {1:^4d} ||".format(m.width_mm,m.height_mm))
+        if m.is_primary==True:
+            print('\n')
+            print('{0:-^120s}'.format(""),) # SEPARATION LINE
+            print("[{0:^19s}][{1:^16s}{2:^6d}][{3:^17s}{4:^6d}][{5:^16s}{6:^6d}][{7:^17s}{8:^6d}]".format(
+                'Primary Display ','Screen Width: ',m.width,'Screen Height: ',m.height,'Width (mm): ',m.width_mm,'Height (mm): ',m.height_mm))
+            print('{0:-^120s}'.format(""),) # SEPARATION LINE
+
         if m.is_primary==False:
             i = 2
-            print("{0:^8s}{1:^6d} || Screen Width: {2:^4d} || Screen Height: {3:^4d}".format('Display #',i,m.width,m.height))
+            print("[{0:^10s}{1:^9d}][{2:^16s}{3:^6d}][{4:^17s}{5:^6d}][{6:^16s}{7:^6d}][{8:^17s}{9:^6d}]".format(
+                'Display',i,'Screen Width: ',m.width,'Screen Height: ',m.height,'Width (mm): ',m.width_mm,'Height (mm): ',m.height_mm))
+            print('{0:-^120s}'.format(""),) # SEPARATION LINE
             i = i+1
-        
+    print('\n')
 if __name__ == '__main__':
     exit(main())
