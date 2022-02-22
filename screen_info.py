4fe176463ad9c4bb2ea7c6db7d169a2da61906d5
DaltonKipp <daltonkipp@gmail.com>
2022-02-22 16:16:54 -0700

    Formatting Changes

--- screen_info.py
+++ screen_info.py
@@ -15,23 +15,23 @@ def main():
     # Prints System Information
     info = platform.uname()
     print("<{0:-^120s}>".format("[ SYSTEM INFO ]"))
-    print("\n[{0:^s}{1:^s}][{2:^s}{3:^s}][{4:^s}{5:^s}][{6:^s}{7:^s}][{8:^s}{9:^s}][{10:^s}{11:^s}]".format(
-        'OS:',info.system,'Device Name:',info.node,'Release:',info.release,'Version:',info.version,'Machine:',info.machine,'Processor:',info.processor))        
+    print("\n[ {0:^s}{1:^s} ]\n[ {2:^s}{3:^s} ]\n[ {4:^s}{5:^s} ]\n[ {6:^s}{7:^s} ]\n[ {8:^s}{9:^s} ]\n[ {10:^s}{11:^s} ]\n".format(
+        'OPERATING SYSTEM: ',info.system,'DEVICE NAME: ',info.node,'RELEASE: ',info.release,'VERSION: ',info.version,'MACHINE: ',info.machine,'PROCESSOR: ',info.processor))        
     
     # Prints Screen Information
     for m in get_monitors():
         if m.is_primary==True:
-            print('\n')
-            print('<{0:-^87s}>'.format(""),)
-            print("[{0:^s}][{1:^s}{2:^d}][{3:^s}{4:^d}][{5:^s}{6:^d}][{7:^s}{8:^d}]".format(
+            #print('\n')
+            print('<{0:-^120s}>'.format(""),)
+            print("[ {0:^s} ][ {1:^s}{2:^d} ][ {3:^s}{4:^d} ][ {5:^s}{6:^d} ][ {7:^s}{8:^d} ]".format(
                 'Primary Display','Screen Width:',m.width,'Screen Height:',m.height,'Width(mm):',m.width_mm,'Height(mm):',m.height_mm))
-            print('<{0:-^87s}>'.format(""))
+            print('<{0:-^120s}>'.format(""))
 
             i = 2
         if m.is_primary==False:
-            print("[{0:^s}{1:^d}][{2:^s}{3:^d}][{4:^s}{5:^d}][{6:^s}{7:^d}][{8:^s}{9:^d}]".format(
+            print("[ {0:^s}{1:^d} ][ {2:^s}{3:^d} ][ {4:^s}{5:^d} ][ {6:^s}{7:^d} ][ {8:^s}{9:^d} ]".format(
                 'Display ',i,'Screen Width:',m.width,'Screen Height:',m.height,'Width(mm):',m.width_mm,'Height(mm):',m.height_mm))
-            print('<{0:-^87s}>'.format(""))
+            print('<{0:-^120s}>'.format(""))
             i = i+1
     print('\n')
     
