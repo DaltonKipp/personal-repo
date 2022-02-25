990e43d1c811d756ee1d497f882064bb11798d0b
DaltonKipp <daltonkipp@gmail.com>
2022-02-24 17:12:59 -0700

    Add USB device info placeholder

--- system_info.py
+++ system_info.py
@@ -1,4 +1,11 @@
+#!
+
+import re
+import subprocess
 import platform
+import sys
+import usb.core
+import usb.util
 import tkinter as tk
 import screeninfo
 from screeninfo import get_monitors
@@ -18,19 +25,34 @@ def main():
         print("\n<{0:-^120s}>".format(istr))
         print('\n'+str(m)+'\n')
     
+    # device_re = re.compile(b"Bus\s+(?P<bus>\d+)\s+Device\s+(?P<device>\d+).+ID\s(?P<id>\w+:\w+)\s(?P<tag>.+)$", re.I)
+    # df = subprocess.check_output("lsusb")
+    # devices = []
+    # for i in df.split('\n'):
+    #     if i:
+    #         dev_info = device_re.match(i)
+    #         if dev_info:
+    #             x = dev_info.match(i)
+    #             x['device'] = '/dev/bus/usb/%s/%s' % (x.pop('bus'), x.pop('device'))
+    # print(devices)
+    
+    # dev = usb.core.find(find_all=True)
+    # for cfg in dev:
+    #     sys.stdout.write('Decimal Vendor ID=' + str(cfg.idVendor) + ' & ProductID=' + str(cfg.idProduct) + '\n')
+    
     # Prints Screen Information
     for m in get_monitors():
         if m.is_primary==True:
             #print('\n')
             print('<{0:-^120s}>'.format(""),)
             print("[ {0:^s} ][ {1:^s}{2:^d} ][ {3:^s}{4:^d} ][ {5:^s}{6:^d} ][ {7:^s}{8:^d} ]".format(
-                'Primary Display','Screen Width:',m.width,'Screen Height:',m.height,'Width(mm):',m.width_mm,'Height(mm):',m.height_mm))
+                'Primary Display','Screen Width: ',m.width,'Screen Height: ',m.height,'Width (mm): ',m.width_mm,'Height (mm): ',m.height_mm))
             print('<{0:-^120s}>'.format(""))
 
             i = 2
         if m.is_primary==False:
             print("[ {0:^s}{1:^d} ][ {2:^s}{3:^d} ][ {4:^s}{5:^d} ][ {6:^s}{7:^d} ][ {8:^s}{9:^d} ]".format(
-                'Display ',i,'Screen Width:',m.width,'Screen Height:',m.height,'Width(mm):',m.width_mm,'Height(mm):',m.height_mm))
+                'Display ',i,'Screen Width: ',m.width,'Screen Height: ',m.height,'Width (mm): ',m.width_mm,'Height (mm): ',m.height_mm))
             print('<{0:-^120s}>'.format(""))
             i = i+1
     print('\n')
