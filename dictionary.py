aecc37061e2fed32386a385c8a83292c1626f176
DaltonKipp <daltonkipp@gmail.com>
2022-03-07 15:12:40 -0700

    Script outputs

--- dictionary.py
+++ dictionary.py
@@ -1,11 +1,27 @@
 # https://pypi.org/project/PyDictionary/
-from PyDictionary import Pydictionary
+from PyDictionary import PyDictionary
+
 dict = PyDictionary()
-def meaning(word):
+
+def mean(word):
     print(dict.meaning(str(word)))
-def synonym(word):
-    print(dict.synonym(str(word)))
-def antonym(word):
-    print(dict.antonym(str(word)))
-def synonym(word,transl):
+
+def syn(word):
+    if dict.synonym(str(word)) == None:
+        print('This word does not have a synonym')
+    else:
+        print(dict.synonym(str(word)))
+
+def ant(word):
+    if dict.antonym(str(word)) == None:
+        print('This word does not have an antonym')
+    else:
+        print(dict.antonym(str(word)))
+        
+def tran(word,transl):
     print(dict.translate(str(word),str(transl)))
+
+word = input('Choose a word: ')
+mean(word) # Prints the meaning of the specified word
+syn(word) # Prints the synonym of the specified word
+ant(word) # Prints the antonym of the specified word
