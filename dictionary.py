cc5b728a706c21e9703e95cbba65eb3b8749269a
DaltonKipp <daltonkipp@gmail.com>
2022-03-07 14:32:45 -0700

    Add dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+from PyDictionary import Pydictionary
+dict = PyDictionary()
+def meaning(word):
+    print(dict.meaning(str(word)))
+def synonym(word):
+    print(dict.synonym(str(word)))
+def antonym(word):
+    print(dict.antonym(str(word)))
+def synonym(word,transl):
+    print(dict.translate(str(word),str(transl)))
