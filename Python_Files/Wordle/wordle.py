4a3541705ebda1a158835828b760fa896b5a307c
DaltonKipp <daltonkipp@gmail.com>
2022-06-29 13:07:02 -0600

    Imports

--- Python_Files/Wordle/wordle.py
+++ Python_Files/Wordle/wordle.py
@@ -1,4 +1,11 @@
 # Wordle Clone
 
+import sys
+sys.path.append('C:/Users/nomad/personal-repo/Python_Files')
 import terminal_formatting as tf
 import colorama
+from rich.console import Console
+from random import choice
+from words import word_list
+
+tf.cl(), tf.dl('[ WORDLE ] ','/','+'), tf.nl()
