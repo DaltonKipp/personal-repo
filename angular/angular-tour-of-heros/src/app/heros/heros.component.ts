86410f70d78cb914566b566ee20fd81d69877eaf
DaltonKipp <daltonkipp@gmail.com>
2022-08-26 13:37:51 -0600

    third part

--- angular/angular-tour-of-heros/src/app/heros/heros.component.ts
+++ angular/angular-tour-of-heros/src/app/heros/heros.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heros';
 
 @Component({
   selector: 'app-heros',
@@ -11,6 +12,12 @@ export class HerosComponent implements OnInit {
     id: 1,
     name: 'Windstorm'
   };
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  heroes = HEROES;
 
   constructor() { }
 
