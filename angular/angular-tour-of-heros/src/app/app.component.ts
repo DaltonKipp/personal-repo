cf179bb6a1ebbbf7d5499d89871bc723e330775f
DaltonKipp <daltonkipp@gmail.com>
2022-08-29 16:36:52 -0600

    Get data from a server beginning

--- angular/angular-tour-of-heros/src/app/app.component.ts
+++ angular/angular-tour-of-heros/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { UpperCasePipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Tour of Heroes';
 }
--- angular/angular-tour-of-heros/src/app/hero.service.ts
+++ angular/angular-tour-of-heros/src/app/hero.service.ts
@@ -3,6 +3,7 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,12 +13,25 @@ export class HeroService {
     this.messageService.add('HeroService: Fetched Heroes')
     return heroes;
   }
-  constructor(private messageService: MessageService) { }
-  getHero(id: number): Observable<Hero> {
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+    ) { }
+
+    getHero(id: number): Observable<Hero> {
     // For now, assume that a hero with the specified `id` always exists.
     // Error handling will be added in the next step of the tutorial.
     const hero = HEROES.find(h => h.id === id)!;
     this.messageService.add(`HeroService: fetched hero id=${id}`);
+    /** Log a HeroService message with the MessageService */
+
     return of(hero);
+  }
+
+  private heroesURL = 'api/heroes'; // URL to web api
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
