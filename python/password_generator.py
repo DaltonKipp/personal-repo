380e7020d02d7665bae4ad52b3f81b6861dba981
Dalton Kipp <dkipp@symbotic.com>
2023-08-10 09:59:14 -0700

    Add destination directory prompt

--- python/password_generator.py
+++ python/password_generator.py
@@ -10,6 +10,7 @@
     - Check password strength
 """
 
+import os
 import string
 import random
 import csv
@@ -26,6 +27,13 @@ yellow = Fore.YELLOW + Style.BRIGHT # Yellow
 green = Fore.GREEN + Style.BRIGHT   # Green
 white = Fore.WHITE + Style.BRIGHT   # White
 reset = Style.RESET_ALL             # Resets all colors
+
+# Checks if destination path exists
+def check(dest):
+    if os.path.exists(dest) == True:
+        dest = dest
+    else:
+        os.mkdir(dest)
 
 def main():
 
@@ -45,7 +53,10 @@ def main():
     length = int(input('\nHow many characters long will the password be? (Suggested 16+): '+yellow))
     # Choose the number of generated passwords
     inst = int(input(reset+'\nHow many different passwords would you like to generate?: '+yellow)) 
-    
+    # Choose destination directory
+    dest = str(input(reset+'\nDestination Deirectory: '+yellow))
+    check(dest)
+
     numb = string.digits        # String of numbers
     char = string.punctuation   # String of special characters
     alph = string.ascii_letters # String of lowercase and uppercase letters
@@ -69,21 +80,21 @@ def main():
                 rx = random.randrange(0,len(store))        # Random slot in store
                 generated_list.append(store[rx])           # Adds characters to the list
         password = green + ''.join(generated_list) + reset # Converts password to a string
-        print('{0:>4d}: {1:s}\n'.format(x,password))       # Prints password and counter
+        print('{0:>4d}: {1:s}\n'.format(x+1,password))       # Prints password and counter
         
         store_passwords.append(''.join(generated_list))                     # Stores all passwords in a list
-    csv_path = '../test_files/pandas_test.csv'                              # Defines file path
+    csv_path = dest+'pandas_test.csv'                             # Defines file path
     dict = {'Password':store_passwords}                                     # .csv data dictionary
     data_frame = pd.DataFrame(dict)                                         # Creates data frame
     data_frame.to_csv(csv_path)                                             # Creates .csv file
     print(yellow+'--> Passwords saved to: "{0:}"\n'.format(csv_path)+reset) # Prints .csv file creation confirmation
 
-    txt_path = '../test_files/test_text_file.txt'                           # .txt file path
+    txt_path = dest+'test_text_file.txt'                           # .txt file path
     text_file = open(txt_path,"w")                                          # Opens new .txt file to write to
     i = 0                                                                   # Iterations
     for element in store_passwords:                                         # Iterates for each item in the list
         i += 1                                                              # Iteration counter
-        text_file.write('['+str(i)+'] - '+element+"\n\n")                   # Writes the element and two new lines
+        text_file.write('['+str(i)+'] - '+element+"\n")                   # Writes the element and two new lines
     text_file.close()                                                       # Closes the .txt file
     print(yellow+'--> Passwords saved to: "{0:}"\n'.format(txt_path)+reset) # Prints .txt file creation confirmation
     tf.dl('  END  ','/','/'), tf.nl() # End Message
