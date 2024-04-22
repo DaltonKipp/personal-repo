304d525d686df53f5bf9f735dad69d0fceb266e6
Dalton Kipp <daltonkipp@gmail.com>
2024-04-21 19:09:06 -0500

    Helix

--- code_art/dna/sketch.js
+++ code_art/dna/sketch.js
@@ -1,37 +1,105 @@
 var CircleNum 
-var Diameter = 20
+var size = 10;
+var amplitude = 180;
+var speed = 100;
+var CircleNum = 1000;
+var cols = 20;
+var radScale = 2;
+var radius = 10;
+let set1 = [];
+let set2 = [];
 
 function setup() { 
   createCanvas(windowWidth, windowHeight);
-  CircleNum = 50;
-
+  spacing = windowWidth / cols;
+  generateCircles(windowHeight/2);
 } 
 
 function draw() { 
-  background(15,15,30,50);
-  
+  background(10,10,10,25);
+  originalDNA()  
+
+  // set1.forEach((Circle, index) => {
+  //   Circle.drawCircle(); // Draw circles
+  //   // Circle.update();     // Update circle position
+  // });
+
+  // set2.forEach((Circle, index) => {
+  //   Circle.drawCircle(); // Draw circles
+  //   // Circle.update();     // Update circle position
+  // });
+}
+
+function originalDNA(){
   for ( i = 0; i < CircleNum; i++) {
-    var g = map(mouseX, 0, 640,100, 255);
-  	var b = map(mouseX, 0, 640, 255, 0);
-  	var x = i*(width)/(CircleNum);
-		fill(random (0,255),g,b,200);
-    noStroke();
-  	ellipse(x,height/2+100*sin(50*i+frameCount/100),Diameter,Diameter);
-    
-  	var x2 = i*(width)/(CircleNum);
-		fill(random (0,255),g,b,200);
-    noStroke();
-  	ellipse(x2,height/2+100*cos(PI*(3/4)+50*i+frameCount/100),Diameter,Diameter);
-    
-    strokeWeight(0.1);
-    stroke(255);
-    line(x,height/2+100*sin(50*i+frameCount/1000),
-      x2,height/2+30*cos(PI*(3/4)+50*i+frameCount/100))
+
+    var x = i*(width)/(CircleNum);
+  	var x2 = (i+1)*(width)/(CircleNum);
+    var x3 = i*(width)/(CircleNum);
+
+    var r = map(x, 0, windowWidth, 255, 255);
+    var g = map(x, 0, windowWidth, 255, 255);
+  	var b = map(x, 0, windowWidth, 255, 255);
       
-		var x3 = i*(width)/(CircleNum);
-		fill(random (0,255),g,b,20);
+    // Falling Circles
+		fill(r,g,b,20);
 		noStroke();
-		ellipse(x3,height/2+100*tan(50*i+frameCount/100),Diameter,Diameter);
-  	
+		ellipse(x,height/2+amplitude*tan(50*i+frameCount/speed/10),2*size,size);
+
+    // Connecting Lines
+    // strokeWeight(0.1);
+    // stroke(r,g,b);
+    // line(x,height/2+amplitude*sin(50*i+frameCount/speed),
+    //   x2,height/2+amplitude*cos(PI*(3/4)+50*i+frameCount/speed))
+      
+    // Red Strand
+    fill(r,0,0,255);
+    noStroke();
+  	ellipse(x,height/2+amplitude*sin(50*i+frameCount/speed),size,size);
+
+    // Cyan Strand
+		fill(0,g,b,255);
+    noStroke();
+  	ellipse(x,height/2+amplitude*cos(PI*(3/4)+50*i+frameCount/speed),size,size);  	
   }
+}
+
+class Circle {
+  constructor(x, y, radius, color) {
+    this.x = x; // X Position
+    this.y = y; // Centerline
+    this.initialX = x; // Store initial position of x
+    this.initialY = y; // Store initial position of y
+    this.initialRad = radius; // Strore initial radius
+    this.radius = radius;
+    this.color = color;
+    this.radSpeed = random(-0.1, 0.1); // Random radius size speed
+  }
+
+  // Draw the circle
+  drawCircle() {
+    fill(this.color); // Ellipse Fill Color
+    ellipse(this.x, this.y, this.radius * radScale, this.radius * radScale); // Ellipse
+    noStroke(); // No Outline
+    // stroke(0);
+  }
+
+  update() {
+    this.y += this.speedY;
+  }
+}
+
+function generateCircles(center) {
+  for(let x=0; x < cols; x++) {
+    let y1 = center + amplitude * sin(x);
+    set1.push(new Circle(x * spacing, y1, radius, [255,0,0]))
+    let y2 = center + amplitude * cos(x);
+    set2.push(new Circle(x * spacing, y2, radius, [0,255,255]))
+  }
+  console.log("set1:",set1);
+  console.log("set2:",set2);
+}
+
+function generateLines() {
+
 }
