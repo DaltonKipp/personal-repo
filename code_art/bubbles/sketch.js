08a5a432960af2a7689b597b47c73445ed9225a1
Dalton Kipp <daltonkipp@gmail.com>
2024-05-02 22:36:52 -0500

    rename directory & updates

--- code_art/bubbles/sketch.js
+++ code_art/bubbles/sketch.js
@@ -1,22 +1,20 @@
-// Dalton Kipp
-
 var noiseArray = []; // Initializes Noise Array Variable
 var colorArray = []; // Initializes Color Array Variable
-let Circles = [];    // Initializes Circle Array Variable
+let Circles = []; // Initializes Circle Array Variable
 
 // Global Variables
-const framerate = 60;     // Capture frame rate
-const noiseScale = 1;     // Sets noise scaling (increase for smoother gradient)
-const detail = 3.0;       // Noise Detail
-const gridScale = 20;     // Sets grid scale (increase for larger grid size)
-const radScale = 5.0;     // Circle radius scale
-const xScale = 0.5;       // X speed scale
-const yScale = 0.5;       // Y Speed Scale
-const maxDistance = 10.0; // Maximum distance scale
-const alpha = 255;        // Circle transparency
-const pad = 10;           // Pad with extra columns and rows
-var xOffset = 0;          // Initial Value Of X Offset
-var yOffset = 0;          // Initial Value Of Y Offset
+const framerate = 60; // Capture frame rate
+const NOISE_SCALE = 1; // Sets noise scaling (increase for smoother gradient)
+const PERLIN_NOISE_DETAIL = 3.0; // Noise Detail
+const GRID_SCALE = 15; // Sets grid scale (increase for larger grid size)
+const RAD_SCALE = 5.0; // Circle radius scale
+const X_SCALE = 0.25; // X speed scale
+const Y_SCALE = 0.25; // Y Speed Scale
+const MAX_DISTANCE = 10.0; // Maximum distance scale
+const ALPHA = 255; // Circle transparency
+const pad = 10; // Pad with extra columns and rows
+var xOffset = 0; // Initial Value Of X Offset
+var yOffset = 0; // Initial Value Of Y Offset
 
 // Create CCapture object
 var capturer = new CCapture({
@@ -28,28 +26,26 @@ var capturer = new CCapture({
 
 function setup() {
 
-  console.log('Window Width =',windowWidth,'Window Height =', windowHeight)
   let p5canvas = createCanvas(windowWidth, windowHeight); // Defines Canvas Size
-  canvas = p5canvas.canvas;
+  canvasCapture = p5canvas.canvas;
   background(0); // Sets Background Color
-  noiseDetail(detail); // Sets Perlin Noise Detail
+  noiseDetail(PERLIN_NOISE_DETAIL); // Sets Perlin Noise Detail
 
-  const cols = windowWidth / gridScale + pad;   // Sets Number of Columns
-  const rows = windowHeight / gridScale + pad;  // Sets Number of Rows
-  console.log('Columns =', cols,'Rows =', rows) // Logs columns and rows to the console
+  const cols = windowWidth / GRID_SCALE + pad;   // Sets Number of Columns
+  const rows = windowHeight / GRID_SCALE + pad;  // Sets Number of Rows
 
   generateNoise(cols, rows);   // Generates Noise Arrays
   generateCircles(cols, rows); // Generates Circle Class Instances
 }
 
 function draw() {
-  background(10)
+  background(10,10,10,10)
   // translate(-windowWidth/2, -windowHeight/2)
   Circles.forEach((Circle, index) => {
     Circle.drawCircle(); // Draw circles
     Circle.update();     // Update circle position
   });
-  capturer.capture(canvas)
+  capturer.capture(canvasCapture)
 }
 
 // Circle Class
@@ -62,8 +58,8 @@ class Circle {
     this.initialRad = circleRadius // Strore initial radius
     this.circleRadius = circleRadius;
     this.circleColor = circleColor;
-    this.speedX = xScale * random(-1,1) // Random speed in X direction
-    this.speedY = yScale * random(-1,1); // Random speed in Y direction
+    this.speedX = X_SCALE * random(-1,1) // Random speed in X direction
+    this.speedY = Y_SCALE * random(-1,1); // Random speed in Y direction
     this.radSpeed = random(-0.1, 0.1); // Random radius size speed
   }
   
@@ -79,12 +75,12 @@ class Circle {
     let distanceX = abs(this.x - this.initialX);
     let distanceY = abs(this.y - this.initialY);
     
-    // Check If The Distance Is Within 10 Percent Of The gridScale
-    if (distanceX > maxDistance * gridScale || distanceY > maxDistance * gridScale) {
+    // Check If The Distance Is Within 10 Percent Of The GRID_SCALE
+    if (distanceX > MAX_DISTANCE * GRID_SCALE || distanceY > MAX_DISTANCE * GRID_SCALE) {
       this.speedX *= -1; // Reverses Direction
       this.speedY *= -1;
     }    
-    // Check If The Distance Is Within 10 Percent Of The gridScale
+    // Check If The Distance Is Within 10 Percent Of The GRID_SCALE
     if (this.circleRadius > this.initialRad * 1.5 || this.circleRadius < this.initialRad * 0.50) {
       this.radSpeed *= -1;
     }
@@ -101,7 +97,7 @@ class Circle {
   // Draw the circle
   drawCircle() {
     fill(this.circleColor); // Ellipse Fill Color
-    ellipse(this.x, this.y, this.circleRadius * radScale, this.circleRadius * radScale); // Ellipse
+    ellipse(this.x, this.y, this.circleRadius * RAD_SCALE, this.circleRadius * RAD_SCALE); // Ellipse
     stroke(0); // No Outline
     // stroke(0);
   }
@@ -122,7 +118,7 @@ function generateNoise(cols, rows) {
   // Assigns Perlin Noise Values to noiseArray Matrix
   for (let x = 0; x < cols; x++) {
     for (let y = 0; y < rows; y++) {
-      noiseArray[x][y] = noise(x / noiseScale, y / noiseScale); // Calculates noise values
+      noiseArray[x][y] = noise(x / NOISE_SCALE, y / NOISE_SCALE); // Calculates noise values
       colorArray[x][y] = map(noiseArray[x][y], 0, 1, 0, 255); // Maps noise values between 0 and 255
     }
   }
@@ -134,9 +130,9 @@ function generateCircles(cols, rows) {
     if (x % 2 != 0) { // For Every Odd Number
       for (let y = 0; y < rows - 1; y++) { // For Every Row
         if (y % 2 != 0) { // For Every Odd Number
-          let circleRadius = 0.5 * gridScale
+          let circleRadius = 0.5 * GRID_SCALE
           let circleColor = getColorBasedOnNoise(x, y); // Get Color Based On Noise
-          Circles.push(new Circle(x * gridScale, y * gridScale, circleRadius, circleColor)); // Push New Circle
+          Circles.push(new Circle(x * GRID_SCALE, y * GRID_SCALE, circleRadius, circleColor)); // Push New Circle
         }
       }
     }
@@ -147,19 +143,19 @@ function generateCircles(cols, rows) {
 function getColorBasedOnNoise(x, y) {
   let noiseValue = colorArray[x][y];
   if (noiseValue <= 80) {
-    return [0, 255, 255, alpha]; // Cyan
+    return [0, 255, 255, ALPHA]; // Cyan
   } else if (noiseValue <= 100) {
-    return [255, 0, 0, alpha]; // Red
+    return [255, 0, 0, ALPHA]; // Red
   } else if (noiseValue <= 120) {
-    return [255, 255, 255, alpha]; // White
+    return [255, 255, 255, ALPHA]; // White
   } else if (noiseValue <= 140) {
-    return [0, 150, 150, alpha]; // Red
+    return [0, 150, 150, ALPHA]; // Red
   } else if (noiseValue <= 160) {
-    return [0, 150, 150, alpha]; // Cyan
+    return [0, 150, 150, ALPHA]; // Cyan
   } else if (noiseValue <= 180) {
-    return [150, 0, 0, alpha]; // Red
+    return [150, 0, 0, ALPHA]; // Red
   } else {
-    return [150, 150, 150, alpha]; //White
+    return [150, 150, 150, ALPHA]; //White
   }
 }
 
--- code_art/neon_rain/sketch.js
+++ code_art/neon_rain/sketch.js
@@ -1,10 +1,10 @@
 let PARTICLES = []; // Creates particle array
 const NUM_PARTICLES = 2000; // Sets number of particles
-const X_SIN_AMPLITUDE = 0.5; // Particle X Sine Motion Amplitude
+const X_SIN_AMPLITUDE = 2.0; // Particle X Sine Motion Amplitude
 const RAD_AMPLITUDE = 0.0; // Particle Sine Radius Size Amplitude
-const BACKGROUND_ALPHA = 5; // Background Alpha (acts like a fade effect)
+const BACKGROUND_ALPHA = 15; // Background Alpha (acts like a fade effect)
 const PARTICLE_ALPHA = 255; // Particle alpha value
-const Y_SPEED_MAX = 1.0; // Maximum y speed
+const Y_SPEED_MAX = 2.0; // Maximum y speed
 
 // Sets up project
 function setup() {
@@ -27,9 +27,9 @@ class Particle {
     this.x = random(0, windowWidth); // Particles start at a random x position
     this.y = random(0, windowHeight); // Particles start at a random y position
     this.radius = random(0, 30); // Particles are a random size
-    this.xAngle = random(0, TWO_PI); // Random angle between 0 and 2pi
+    this.xAngle = sin(this.y); // Random angle between 0 and 2pi
     this.angle2 = random(0, TWO_PI); // Random angle between 0 and 2pi
-    this.xSpeed = X_SIN_AMPLITUDE * sin(this.y);
+    this.xSpeed = X_SIN_AMPLITUDE * this.xAngle;
     this.ySpeed = random(Y_SPEED_MAX/4, Y_SPEED_MAX); // Particles travel at a random speed
   }
 
@@ -38,22 +38,22 @@ class Particle {
     this.radius += RAD_AMPLITUDE * sin(this.angle2);
     this.x += this.xSpeed;
     this.y += this.ySpeed;
-    this.xAngle += 0.1;
-    this.angle2 += 0.01;
 
-    if (this.y >= windowHeight + this.radius * 2) {
-      this.y = -this.radius * 2;
+    if (this.y <= -this.radius*2 || this.y >= windowHeight + this.radius*2) {
+      this.ySpeed *= -1; // Reverses y direction if outside of the boundaries
+    }
+    if (this.x <= -this.radius*2 || this.x >= windowWidth + this.radius*2) {
+      this.xSpeed *= -1; // Reverses x direction if outside of the boundaries
     }
   }
 
   // Draws the particles
   draw() {
-    if (this.radius > 10 && this.radius <= 20) {
-      this.color = [255, 0, 0, PARTICLE_ALPHA];
-    } else if (this.radius > 20 && this.radius <= 30) {
-      this.color = [0, 255, 255, PARTICLE_ALPHA];
-    } else { this.color = [255, 255, 255, PARTICLE_ALPHA];
-    }
+    if (this.radius > 10 && this.radius <= 20)
+      {this.color = [255, 0, 0, PARTICLE_ALPHA];}
+    else if (this.radius > 20 && this.radius <= 30)
+      {this.color = [0, 255, 255, PARTICLE_ALPHA];}
+    else {this.color = [255, 255, 255, PARTICLE_ALPHA];}
 
     fill(this.color); // Fills each particle
     ellipse(this.x, this.y, this.radius);
