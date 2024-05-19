577983e6c22e30739fb56c0b4155f081d55c59cc
DaltonKipp <daltonkipp@gmail.com>
2024-05-18 20:14:55 -0500

    hex moon

--- code_art/hex_gradient/hex_gradient.js
+++ code_art/hex_gradient/hex_gradient.js
@@ -1,71 +1,83 @@
 let RADIUS = 10; // Sets hexagon radius
-let SIDE_LENGTH = Math.sqrt((3 * Math.pow(RADIUS, 2)) / 4);
+let SIDE_LENGTH = Math.sqrt((3 * Math.pow(RADIUS, 2)) / 4); // Calculates Side length
 let hexagons = [];
 
 function setup() {
-	canvasWidth = windowWidth;
-	canvasHeight = windowHeight;
-	colorMode(RGBA) // Defines color mode
+	canvasWidth = windowWidth; // Sets canvas width
+	canvasHeight = windowHeight; // Sets canvas height
+	colorMode(RGBA); // Defines color mode
 	createCanvas(canvasWidth, canvasHeight); // Creates canvas
-	generateHexagons();
+	generateHexagons(); // Populates Hexagon array
+	noiseDetail(3); // Sets perlin noise detail value
+	frameRate(60)
 }
 
 // Draws Hexagon
 function draw() {
 	background(0);
 	hexagons.forEach((hexagon) => {
-		hexagon.color();
-		hexagon.drawPolygon(6);
+		hexagon.color(); // Gets hexagon color
+		hexagon.drawPolygon(6); // Draws hexagons
+		hexagon.update(); // Updates hexagon parameters
 	});
-	noLoop();
 }
 
 // Hexagon Class
 class Hexagon {
 	constructor(x, y, r) {
-		// Sets up x position, y position, radius
 		this.x = x;
 		this.y = y;
 		this.r = r;
+		this.noiseOffset = 0;
+		this.delta = 1;
+		// Calculates distance from the polygon to the middle of the canvas
 		this.distanceFromCenter = dist(x, y, canvasWidth / 2, canvasHeight / 2);
+		// Calculates maximum distance value
+		this.maxDistance = dist(0, 0, canvasWidth / 2, canvasHeight / 2);
+		// Calculates ratio of dictance from center & max distance
+		this.colorFactor = this.distanceFromCenter / this.maxDistance;
+		// Calculates base perlin noise value
+		this.baseNoiseValue = noise(this.x + this.noiseOffset, this.y);
 	}
 	
   // Renders Hexagon
 	color() {
-		// Calculate color based on distance from center
-		let maxDistance = dist(0, 0, canvasWidth / 2, canvasHeight / 2);
-		let colorFactor = this.distanceFromCenter / maxDistance;
-		
-		let RED = 255 * noise(this.x, this.y);
-		let GREEN = 255 * noise(this.x, this.y);
-		let BLUE = 255 * noise(this.x, this.y);
+		// Calculate color based on precomputed values
+		let noiseValue = noise(this.x + this.noiseOffset, this.y + this.noiseOffset);
+		let RED = 500 * noiseValue * (1 - this.colorFactor);
+		let GREEN = 500 * noiseValue * (1 - this.colorFactor);
+		let BLUE = 500 * noiseValue * (1 - this.colorFactor);
 
 		// Sets the Fill of the Hexagons
-		if (this.distanceFromCenter < canvasWidth / 4) {
+		if (this.colorFactor >= 0.0 && this.colorFactor < 0.20) {
 			fill(RED, 0, 0);
+		} else if (this.colorFactor >= 0.20 && this.colorFactor < 0.25) {
+			fill(RED - 50, 0, 0); // Slightly darker
+		} else if (this.colorFactor >= 0.25 && this.colorFactor <= 0.30) {
+			fill(RED - 100, 0, 0); // Slightly darker
+		} else {
+			fill(RED);
 		}
-		else if (this.distanceFromCenter < canvasWidth / 3) {
-			fill(0, GREEN, 0);
-		}
-		else if (this.distanceFromCenter < canvasWidth / 2.5) {
-			fill(0, 0, BLUE);
-		}		else {
-			fill(RED, GREEN, BLUE, 255);
-		}
-		
-		// Sets the Stroke of the Hexagons
-		stroke(0);
-		strokeWeight(1);
 	}
 
 	drawPolygon(numSides) {
+		const angleIncrement = (2 * Math.PI) / numSides;
 		beginShape();
-		for (let a = 0; a < 2 * PI; a += (2 * PI) / numSides) {
-			let x2 = cos(a) * this.r;
-			let y2 = sin(a) * this.r;
+		for (let a = 0; a < 2 * Math.PI; a += angleIncrement) {
+			let x2 = Math.cos(a) * this.r;
+			let y2 = Math.sin(a) * this.r;
 			vertex(this.x + x2, this.y + y2);
 		}
 		endShape(CLOSE);
+
+		// Sets the Stroke of the Hexagons
+		stroke(0)
+		strokeWeight(2)
+	}
+
+	update() {
+		this.noiseOffset += 0.05;
+		//this.baseNoiseValue = noise(this.x + this.noiseOffset, this.y);
 	}
 }
 
@@ -79,5 +91,4 @@ function generateHexagons() {
 			hexagons.push(new Hexagon(x + 1.5 * RADIUS, y + SIDE_LENGTH, RADIUS));
 		}
 	}
-	console.log('Hexagon Array:', hexagons);
 }
