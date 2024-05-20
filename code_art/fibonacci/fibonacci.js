0594a27c028a88000e63c0a2e0cc4410e30512cd
DaltonKipp <daltonkipp@gmail.com>
2024-05-20 06:08:54 -0500

    Formatting, redfine variables, animation tweaks

--- code_art/fibonacci/fibonacci.js
+++ code_art/fibonacci/fibonacci.js
@@ -7,8 +7,8 @@ const PHI = (1 + Math.sqrt(5)) / 2; // Global calulation of PHI
 const nth = 4000; // nth degree of the drawSpiral iterations
 let polySides = 6; // Set default number of polygon sides
 
-let globalRotationAngle = 0; // Add a global variable for the overall rotation
-let polygonRotationAngle = 0; // Add a global variable for the polygon rotation
+var GLOBAL_ROTATION_ANGLE = 0; // Add a global variable for the overall rotation
+var POLYGON_ROTATION_ANGLE = 0; // Add a global variable for the polygon rotation
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -18,23 +18,22 @@ function setup() {
 function draw() {
   background(0, BACKGROUND_ALPHA); // Refresh Background
   translate(windowWidth / 2, windowHeight / 2); // Move origin to center of canvas
-  rotate(globalRotationAngle); // Apply the global rotation
-  noStroke();
+  rotate(GLOBAL_ROTATION_ANGLE); // Apply the global rotation to the canvas
   drawSpiral();
 
-  globalRotationAngle += 0.1; // Increment the global rotation angle
-  polygonRotationAngle += 1; // Increment the polygon rotation angle
+  GLOBAL_ROTATION_ANGLE += 0.1; // Increment the global rotation angle
+  POLYGON_ROTATION_ANGLE += 1; // Increment the polygon rotation angle
 }
 
 function drawSpiral() {
   for (let i = 1; i < nth; i++) {
-    let f = i * SPIRAL_SCALE / nth;
-    let angle = i * ANGLE_SCALE * PHI;
-    let radius = f * RAD_SCALE;
-    let distance = f * radius;
+    var f = (i * SPIRAL_SCALE) / nth;
+    var angle = i * ANGLE_SCALE * PHI;
+    var radius = f * RAD_SCALE;
+    var distance = f * radius;
 
-    let x = DIST_SCALE * cos(angle * TWO_PI) * distance;
-    let y = DIST_SCALE * sin(angle * TWO_PI) * distance;
+    var x = DIST_SCALE * cos(angle * TWO_PI) * distance;
+    var y = DIST_SCALE * sin(angle * TWO_PI) * distance;
 
     // Set the number of polygon sides based on the index
     if (i % 2 == 0) {
@@ -46,38 +45,44 @@ function drawSpiral() {
     }
 
     // Calculate the distance from the center of the canvas
-    let distFromCenter = dist(0, 0, x, y);
+    var distFromCenter = dist(0, 0, x, y);
     // Map the distance to a color range
-    let maxDist = dist(0, 0, width / 2, height / 2);
-    let colorValue = map(distFromCenter, 0, maxDist, 0, 1);
-    let col;
+    var maxDist = dist(0, 0, width / 2, height / 2); // Calculates maximum distance
+    var colorValue = map(distFromCenter, 0, maxDist, 0, 1); // Calculates colorValue
+    var col; // Initializes color variable
 
-    if (colorValue < 0.5) {
+    // Map colors based on the distance from the center
+    if (colorValue <= 0.33) {
+      // Black to Red
+      let t = map(colorValue, 0, 0.33, 0, 1);
+      col = lerpColor(color(0, 0, 0), color(255, 0, 0), t);
+    } else if (colorValue <= 0.66) {
       // Red to Cyan
-      let t = map(colorValue, 0, 0.5, 0, 1);
-      col = lerpColor(color(155, 0, 0), color(0, 255, 255), t);
+      let t = map(colorValue, 0.33, 0.66, 0, 1);
+      col = lerpColor(color(255, 0, 0), color(0, 255, 255), t);
     } else {
       // Cyan to White
-      let t = map(colorValue, 0.5, 1, 0, 1);
+      let t = map(colorValue, 0.66, 1, 0, 1);
       col = lerpColor(color(0, 255, 255), color(255, 255, 255), t);
     }
 
     fill(col);
-    drawPolygon(x, y, radius, polySides, polygonRotationAngle + i); // Pass the rotation angle to drawPolygon
+    drawPolygon(x, y, radius, polySides, POLYGON_ROTATION_ANGLE + i); // Pass the rotation angle to drawPolygon
   }
 }
 
 function drawPolygon(x, y, radius, nPoints, rotationAngle) {
   let angle = 360 / nPoints;
-  push();
-  translate(x, y);
+  push(); // Initialize transformations
+  translate(x, y); // Translate to given x y position
   rotate(rotationAngle); // Apply individual polygon rotation
-  beginShape();
+  beginShape(); // Initialize shape
   for (let a = 0; a < 360; a += angle) {
     let sx = cos(a) * radius;
     let sy = sin(a) * radius;
     vertex(sx, sy);
   }
-  endShape(CLOSE);
-  pop();
+  endShape(CLOSE); // End shape
+  pop(); // Store transformations
+  noStroke(); //
 }
--- code_art/hex_gradient/hex_gradient.js
+++ code_art/hex_gradient/hex_gradient.js
@@ -1,94 +1,95 @@
 let RADIUS = 10; // Sets hexagon radius
 let SIDE_LENGTH = Math.sqrt((3 * Math.pow(RADIUS, 2)) / 4); // Calculates Side length
 let hexagons = [];
+let canvasWidth, canvasHeight;
 
 function setup() {
-	canvasWidth = windowWidth; // Sets canvas width
-	canvasHeight = windowHeight; // Sets canvas height
-	colorMode(RGBA); // Defines color mode
-	createCanvas(canvasWidth, canvasHeight); // Creates canvas
-	generateHexagons(); // Populates Hexagon array
-	noiseDetail(3); // Sets perlin noise detail value
-	frameRate(60)
+  canvasWidth = windowWidth; // Sets canvas width
+  canvasHeight = windowHeight; // Sets canvas height
+  colorMode(RGB); // Defines color mode
+  createCanvas(canvasWidth, canvasHeight); // Creates canvas
+  generateHexagons(); // Populates Hexagon array
+  noiseDetail(3); // Sets perlin noise detail value
+  frameRate(60);
 }
 
 // Draws Hexagon
 function draw() {
-	background(0);
-	hexagons.forEach((hexagon) => {
-		hexagon.color(); // Gets hexagon color
-		hexagon.drawPolygon(6); // Draws hexagons
-		hexagon.update(); // Updates hexagon parameters
-	});
+  background(0);
+  hexagons.forEach((hexagon) => {
+    hexagon.color(); // Gets hexagon color
+    hexagon.drawPolygon(6); // Draws hexagons
+    hexagon.update(); // Updates hexagon parameters
+  });
 }
 
 // Hexagon Class
 class Hexagon {
-	constructor(x, y, r) {
-		this.x = x;
-		this.y = y;
-		this.r = r;
-		this.noiseOffset = 0;
-		this.delta = 1;
-		// Calculates distance from the polygon to the middle of the canvas
-		this.distanceFromCenter = dist(x, y, canvasWidth / 2, canvasHeight / 2);
-		// Calculates maximum distance value
-		this.maxDistance = dist(0, 0, canvasWidth / 2, canvasHeight / 2);
-		// Calculates ratio of dictance from center & max distance
-		this.colorFactor = this.distanceFromCenter / this.maxDistance;
-		// Calculates base perlin noise value
-		this.baseNoiseValue = noise(this.x + this.noiseOffset, this.y);
-	}
-	
+  constructor(x, y, r) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.noiseOffset = 0;
+    this.delta = 1;
+    // Calculates distance from the polygon to the middle of the canvas
+    this.distanceFromCenter = dist(x, y, canvasWidth / 2, canvasHeight / 2);
+    // Calculates maximum distance value
+    this.maxDistance = dist(0, 0, canvasWidth / 2, canvasHeight / 2);
+    // Calculates ratio of dictance from center & max distance
+    this.colorFactor = this.distanceFromCenter / this.maxDistance;
+  }
+
   // Renders Hexagon
-	color() {
-		// Calculate color based on precomputed values
-		let noiseValue = noise(this.x + this.noiseOffset, this.y + this.noiseOffset);
-		let RED = 500 * noiseValue * (1 - this.colorFactor);
-		let GREEN = 500 * noiseValue * (1 - this.colorFactor);
-		let BLUE = 500 * noiseValue * (1 - this.colorFactor);
+  color() {
+    // Calculate color based on precomputed values
+    let noiseValue = noise(
+      this.x + this.noiseOffset,
+      this.y + this.noiseOffset
+    );
+    var RED = 500 * noiseValue * (1 - this.colorFactor);
+    var GREEN = 500 * noiseValue * (1 - this.colorFactor);
+    var BLUE = 500 * noiseValue * (1 - this.colorFactor);
 
-		// Sets the Fill of the Hexagons
-		if (this.colorFactor >= 0.00 && this.colorFactor < 0.20) {
-			fill(RED, 25, 0);
-		} else if (this.colorFactor >= 0.20 && this.colorFactor < 0.25) {
-			fill(RED - 50, 0, 0); // Slightly darker
-		} else if (this.colorFactor >= 0.25 && this.colorFactor <= 0.30) {
-			fill(RED - 100, 0, 0); // Slightly darker
-		} else {
-			fill(RED-25);
-		}
-	}
+    // Sets the Fill of the Hexagons
+    if (this.colorFactor >= 0.0 && this.colorFactor < 0.2) {
+      fill(RED, 25, 0);
+    } else if (this.colorFactor >= 0.2 && this.colorFactor < 0.25) {
+      fill(RED - 50, 0, 0); // Slightly darker
+    } else if (this.colorFactor >= 0.25 && this.colorFactor <= 0.3) {
+      fill(RED - 100, 0, 0); // Slightly darker
+    } else {
+      fill(RED - 25);
+    }
+  }
 
-	drawPolygon(numSides) {
-		const angleIncrement = (2 * Math.PI) / numSides;
-		beginShape();
-		for (let a = 0; a < 2 * Math.PI; a += angleIncrement) {
-			let x2 = Math.cos(a) * this.r;
-			let y2 = Math.sin(a) * this.r;
-			vertex(this.x + x2, this.y + y2);
-		}
-		endShape(CLOSE);
+  drawPolygon(numSides) {
+    var angleIncrement = (2 * Math.PI) / numSides;
+    beginShape();
+    for (let a = 0; a < 2 * Math.PI; a += angleIncrement) {
+      let x2 = Math.cos(a) * this.r;
+      let y2 = Math.sin(a) * this.r;
+      vertex(this.x + x2, this.y + y2);
+    }
+    endShape(CLOSE);
 
-		// Sets the Stroke of the Hexagons
-		stroke(0)
-		strokeWeight(2)
-	}
+    // Sets the Stroke of the Hexagons
+    stroke(0);
+    strokeWeight(2);
+  }
 
-	update() {
-		this.noiseOffset += 0.05;
-		//this.baseNoiseValue = noise(this.x + this.noiseOffset, this.y);
-	}
+  update() {
+    this.noiseOffset += 0.01;
+  }
 }
 
 function generateHexagons() {
-	// create hexagons
-	for (let y = 0; y < canvasHeight + SIDE_LENGTH; y += 2 * SIDE_LENGTH) {
-		for (let x = 0; x < canvasWidth + RADIUS; x += 3 * RADIUS) {
-			// Creates first row of hexagons
-			hexagons.push(new Hexagon(x, y, RADIUS));
-			// Creates second row of hexagons with offset
-			hexagons.push(new Hexagon(x + 1.5 * RADIUS, y + SIDE_LENGTH, RADIUS));
-		}
-	}
+  // Create hexagon grid
+  for (let y = 0; y < windowHeight + SIDE_LENGTH; y += 2 * SIDE_LENGTH) {
+    for (let x = 0; x < windowWidth + RADIUS; x += 3 * RADIUS) {
+      // Creates first row of hexagons
+      hexagons.push(new Hexagon(x, y, RADIUS));
+      // Creates second row of hexagons with offset
+      hexagons.push(new Hexagon(x + 1.5 * RADIUS, y + SIDE_LENGTH, RADIUS));
+    }
+  }
 }
