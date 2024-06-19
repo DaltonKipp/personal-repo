cd56372c319a9ba34b22f4130fe2d8b6ce6166e9
DaltonKipp <daltonkipp@gmail.com>
2024-06-18 23:19:36 -0600

    Comments and value changes

--- code_art/examples/particle_system/sketch.js
+++ code_art/examples/particle_system/sketch.js
@@ -2,29 +2,30 @@
 // Video Link: https://www.youtube.com/watch?v=5dIbK0auaB8&list=PLsPnqXfykr1cMSF0ew3NS4hAx0NwfYnOA&index=4&ab_channel=Frankslaboratory
 
 // setup
-const canvas = document.getElementById("canvas1");
-const ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+const canvas = document.getElementById("canvas1"); // Defines Canvas
+const ctx = canvas.getContext("2d"); // Defines 2D Context
+canvas.width = window.innerWidth; // Sets canvas width to window width
+canvas.height = window.innerHeight; // Sets canvas height to window height
 
-console.log(ctx);
-const gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
-gradient.addColorStop(0, "white");
-gradient.addColorStop(0.5, "gold");
-gradient.addColorStop(1, "orangered");
-ctx.fillStyle = gradient;
-ctx.strokeStyle = "white";
+console.log(ctx); // Logs canvas parameter
+const gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height); // Creates linear color gradient
+// Defines color gradient stops
+gradient.addColorStop(0, "blue");
+gradient.addColorStop(0.5, "purple");
+gradient.addColorStop(1, "red");
+ctx.fillStyle = gradient; // Sets global fill style
+ctx.strokeStyle = "white"; // Sets global stroke style
 
 class Particle {
   constructor(effect) {
     this.effect = effect;
-    this.radius = Math.floor(Math.random() * 10 + 1);
+    this.radius = Math.floor(Math.random() * 20 + 1);
     this.x =
       this.radius + Math.random() * (this.effect.width - this.radius * 2);
     this.y =
       this.radius + Math.random() * (this.effect.height - this.radius * 2);
-    this.vx = Math.random() * 1 - 0.5;
-    this.vy = Math.random() * 1 - 0.5;
+    this.vx = Math.random() * 0.5 - 0.25;
+    this.vy = Math.random() * 0.5 - 0.25;
     this.pushX = 0;
     this.pushY = 0;
     this.friction = 0.95;
@@ -81,7 +82,7 @@ class Effect {
     this.width = this.canvas.width;
     this.height = this.canvas.height;
     this.particles = [];
-    this.numberOfParticles = 300;
+    this.numberOfParticles = 1000;
     this.createParticles();
 
     this.mouse = {
@@ -109,11 +110,13 @@ class Effect {
       this.mouse.pressed = false;
     });
   }
+  // Creates particle class instances
   createParticles() {
     for (let i = 0; i < this.numberOfParticles; i++) {
       this.particles.push(new Particle(this));
     }
   }
+  // Draws Particles and Lines
   handleParticles(context) {
     this.connectParticles(context);
     this.particles.forEach((particle) => {
@@ -121,8 +124,9 @@ class Effect {
       particle.update();
     });
   }
+  // Connects Particles With a Line
   connectParticles(context) {
-    const maxDistance = 80;
+    const maxDistance = 100;
     for (let a = 0; a < this.particles.length; a++) {
       for (let b = a; b < this.particles.length; b++) {
         const dx = this.particles[a].x - this.particles[b].x;
@@ -141,15 +145,16 @@ class Effect {
       }
     }
   }
+  // Handles resizing of the window
   resize(width, height) {
     this.canvas.width = width;
     this.canvas.height = height;
     this.width = width;
     this.height = height;
     const gradient = this.context.createLinearGradient(0, 0, width, height);
-    gradient.addColorStop(0, "white");
-    gradient.addColorStop(0.5, "gold");
-    gradient.addColorStop(1, "orangered");
+    gradient.addColorStop(0, "blue");
+    gradient.addColorStop(0.5, "purple");
+    gradient.addColorStop(1, "red");
     this.context.fillStyle = gradient;
     this.context.strokeStyle = "white";
     this.particles.forEach((particle) => {
